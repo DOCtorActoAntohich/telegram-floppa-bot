18779e58cb5a4c7d41fbaa5bfd4d562077186e13
DOCtorActoAntohich <fairy666death@mail.ru>
2022-07-26 23:23:44 +0700

    Fix this DUMB type annotation

--- floppa/telegram/bot.py
+++ floppa/telegram/bot.py
@@ -1,12 +1,9 @@
-from typing import Callable, Coroutine, Concatenate, TypeVar, ParamSpec
 import datetime
+from typing import Callable, Coroutine
 
 import aiogram  # type: ignore
 
 from floppa.settings import Settings
-
-
-P = ParamSpec("P")
 
 
 class _Bot:
@@ -31,7 +28,7 @@ class _Bot:
     def startup_time(self) -> datetime.datetime:
         return self.__startup_time
 
-    def run(self, *, on_startup: Callable[Concatenate[aiogram.Dispatcher, P], Coroutine[None, None, None]]) -> None:
+    def run(self, *, on_startup: Callable[[aiogram.Dispatcher], Coroutine[None, None, None]]) -> None:
         aiogram.executor.start_polling(self.__dispatcher, on_startup=on_startup, skip_updates=True)
 
 
