fb465467b89c27f8cc59f22bc6b2e6f0730930d8
DOCtorActoAntohich <fairy666death@mail.ru>
2022-07-29 21:40:28 +0700

    Epic sudo removal

--- run_tests.sh
+++ run_tests.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 
-if [ "$EUID" -ne 0 ]
-then
-  echo "You must have root access."
-  exit 1
-fi
-
 
 echo "Starting containers. Please, wait..."
-# docker compose -f=docker-compose-test.yml up --build --abort-on-container-exit --quiet-pull
-docker compose -f=docker-compose-test.yml up --build --abort-on-container-exit
+docker compose -f=docker-compose-test.yml up --build --abort-on-container-exit --quiet-pull
 
 compose_up_return_code=$?
 if [ $compose_up_return_code -eq 0 ]
