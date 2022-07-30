9ec50e28f2c4861b4b6b13c6b2d92f4d17994032
DOCtorActoAntohich <fairy666death@mail.ru>
2022-07-30 20:08:09 +0700

    Add command restrictions to avoid a couple of If-statements

--- floppa/settings/bot.py
+++ floppa/settings/bot.py
@@ -4,3 +4,4 @@ from pydantic import BaseSettings, Field
 class _BotSettings(BaseSettings):
     token: str = Field(..., env="BOT_TOKEN")
     alias: str = Field(..., env="BOT_ALIAS")
+    creator_ids: list[int] = Field(..., env="BOT_CREATOR_IDS")
