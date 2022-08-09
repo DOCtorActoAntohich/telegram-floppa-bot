3046c366da656ff0f533ff2fee8893967f055681
DOCtorActoAntohich <fairy666death@mail.ru>
2022-08-09 11:44:38 +0300

    Switch to custom command argument parsing

--- floppa/telegram/commands/custom/del_command.py
+++ floppa/telegram/commands/custom/del_command.py
@@ -1,6 +1,6 @@
 from aiogram.types import Message  # type: ignore
 
-from floppa.models import ProtectedCommand
+from floppa.models import Command, ProtectedCommand
 from floppa.telegram import floppa_bot
 from floppa.telegram.command_restrictions import CommandRestriction
 from floppa.use_case import DeleteCommandUseCase
@@ -9,6 +9,7 @@ from floppa.use_case import DeleteCommandUseCase
 @floppa_bot.dispatcher.message_handler(ProtectedCommand.del_command.filter)
 @CommandRestriction.only_for_chat_admins
 async def delete_custom_command(message: Message) -> None:
-    use_case = DeleteCommandUseCase(message.get_args(), message.chat.id)
+    args = Command.parse_args(message.text)
+    use_case = DeleteCommandUseCase(args, message.chat.id)
     response = await use_case.execute()
     await message.reply(response)
